d09e9837f939091a10bbec4af5af7833e14e0c30
jeremyda173 <jh4017034@gmail.com>
2024-01-29 19:27:46 -0400

    fix: uncommenting

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,6 +11,7 @@ import {
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
+
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
@@ -34,12 +35,14 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+  // Fetch the last 5 invoices, sorted by date
+  const data = await sql<LatestInvoiceRaw>`
+  SELECT invoices.amount, customers.name, customers.image_url, customers.email
+  FROM invoices
+  JOIN customers ON invoices.customer_id = customers.id
+  ORDER BY invoices.date DESC
+  LIMIT 5`;
+  
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
