569da7500589d8ef5022858a626112b5aeae3881
jeremyda173 <jh4017034@gmail.com>
2024-03-09 19:06:15 -0400

    refactor: reorganize code

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -5,7 +5,8 @@ import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
 import { fetchLatestInvoices } from '@/app/lib/data';
 
-export default async function LatestInvoices({latestInvoices}: {latestInvoices: LatestInvoice[];}) {
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
   
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
